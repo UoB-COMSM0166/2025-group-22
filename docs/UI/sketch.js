1de87a8904aece3c38983a7d9da793393621c694
YCC317 <yxli5327@gmail.com>
2025-02-07 17:19:47 +0000

    Build Start Interface

---
+++ docs/UI/sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(800, 450);
+  background(180, 217, 239);
+
+}
+
+function draw() {
+
+  let width = 150;
+  let height = 90;
+  let buttonY = 320;//450-60-50
+
+  button(80, buttonY, width, height, "Level");
+  button(330, buttonY, width, height, "Start");
+  button(580, buttonY, width, height, "Exit");
+
+  textFormat(400, 150, 80, "2D Jumper");
+
+}
+
+function button(x, y, w, h, label){
+  fill(245, 242, 196);
+  noStroke();
+  rect(x, y, w, h, 10);
+  fill(0);
+  textSize(28);
+  textAlign(CENTER, CENTER);
+  text(label, x + w / 2, y + h / 2);
+}
+
+function textFormat(x, y, size, label){
+  textSize(size);
+  textStyle(BOLD);
+  textFont('Courier New');
+  fill(0);
+  text(label, x, y)
+}
