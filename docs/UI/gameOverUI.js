0da8cb8cbd7f04b26cfc41f04c70e69a5682a6b6
YCC317 <yxli5327@gmail.com>
2025-02-07 17:37:42 +0000

    Build Game Over Interface

---
+++ docs/UI/gameOverUI.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(800, 450);
+  background(180, 217, 239);
+
+}
+
+function draw() {
+
+  let width = 150;
+  let height = 90;
+  let buttonY = 320;//450-60-50
+
+  button(170, buttonY, width, height, "Restart");
+  button(480, buttonY, width, height, "Exit");
+
+  textFormat(400, 150, 80, "Game Over");
+
+}
+
+function button(x, y, w, h, label){
+  fill(245, 242, 196);
+  noStroke();
+  rect(x, y, w, h, 10);
+  fill(0);
+  textSize(28);
+  textAlign(CENTER, CENTER);
+  text(label, x + w / 2, y + h / 2);
+}
+
+function textFormat(x, y, size, label){
+  textSize(size);
+  textStyle(BOLD);
+  textFont('Courier New');
+  fill(0);
+  text(label, x, y)
+}
